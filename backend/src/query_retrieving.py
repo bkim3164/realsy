0e251daddc5d8292e9d482039ed4e3baac7a184a
Radhakrishna Vempati <vempatir@uci.edu>
2023-02-05 03:35:33 -0800

    Commited with image Source FIles

--- backend/src/query_retrieving.py
+++ backend/src/query_retrieving.py
@@ -5,8 +5,6 @@ import query_sending
 db_url = 'postgresql://Realsy:JmUsz3HAW98i65TP389vPA@forest-grizzly-4869.6wr.cockroachlabs.cloud:26257/forest-grizzly-4869.defaultdb?sslmode=verify-full'
 
 def main(location, x = False):
-    print(f'Given Location type:{ type(location)}')
-    print(f"given Location: {location}")
     #this will be the input in the future
 
     #create the connection
@@ -14,30 +12,36 @@ def main(location, x = False):
     cursor = connection.cursor()
 
     #execute retrieval from database
-    cursor.execute("SELECT name,firm,number,experience,sold FROM agents WHERE location = %s;",(location,))
+    cursor.execute("SELECT name,firm,number,experience,sold,image FROM agents WHERE location = %s;",(location,))
     results = cursor.fetchall()
 
 
     if len(results) == 0 and x == False:
         print('Info not found in database, trying to retrieve from website...')
         city, state = location.split(',')
-        city = city.replace(' ', '_')
+        city = city.replace(' ', '-')
         scrapped_info: dict = agent_data_scrapper.main(city, state)
         query_sending.main(scrapped_info)
-        main(location, True)
+        return main(location, True)
+
+
+    if len(results) == 0 and x == True:
+        print("Invalid Input, no info found!")
+        return None
+        
 
     print("Sucessfully retrieved information from the database!")
 
 
     # THIS is for debugging
 
-    #for result in results:
-    #    print(result)
+    for result in results:
+       print(result)
 
     return results
 
 
 if __name__ == '__main__':
-    i_location = 'irvine,CA'
+    i_location = 'irvine,ca'
     main(i_location)
 
--- backend/src/query_sending.py
+++ backend/src/query_sending.py
@@ -11,14 +11,16 @@ def main(agents_data: dict):
     cursor = connection.cursor()
 
     #table create initalize
-    table_query = "CREATE TABLE IF NOT EXISTS Agents (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), name STRING, firm STRING, number STRING, experience STRING, sold INT4, location STRING);"
-
+    table_query = "CREATE TABLE IF NOT EXISTS Agents (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), name STRING, firm STRING, number STRING, experience STRING, sold INT4, location STRING, image STRING);"
+    cursor.execute(table_query)
+    connection.commit()
+    
 
     #input values in table
-    order = "name,firm,number,sold,experience,location"
+    order = "name,firm,number,sold,experience,location,image"
     for agent_info in agents_data.items():
         #print(agent_info[1])
-        cursor.execute(f"INSERT INTO Agents ({order}) VALUES (%s,%s,%s,%s,%s,%s);",agent_info[1])
+        cursor.execute(f"INSERT INTO Agents ({order}) VALUES (%s,%s,%s,%s,%s,%s,%s);",agent_info[1])
 
     
     connection.commit()
@@ -26,5 +28,6 @@ def main(agents_data: dict):
 
 
 if __name__ == "__main__":
-    pass
+    all_data = agent_data_scrapper.main("irvine", "ca")
+    main(all_data)
         
